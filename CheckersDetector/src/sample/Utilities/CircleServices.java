f029f531c950d81909d57790c17c9f6fb5ca7c73
no.nowak@outlook.com <no.nowak@outlook.com>
2017-04-17 15:21:37 +0200

    Created class

---
+++ CheckersDetector/src/sample/Utilities/CircleServices.java
@@ -0,0 +1,16 @@
+package sample.Utilities;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import static org.opencv.imgproc.Imgproc.HoughCircles;
+
+public class CircleServices {
+
+    public void findCircles(Mat src){
+        Mat gray = Core.inRange(src, );
+        HoughCircles()
+
+    }
+}
