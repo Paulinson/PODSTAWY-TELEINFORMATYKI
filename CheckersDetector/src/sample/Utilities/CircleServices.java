ac3e25b508f8243afae2aa2e85c204b751e2ba5a
no.nowak@outlook.com <no.nowak@outlook.com>
2017-04-19 22:03:27 +0200

    test

--- CheckersDetector/src/sample/Utilities/CircleServices.java
+++ CheckersDetector/src/sample/Utilities/CircleServices.java
@@ -1,32 +1,48 @@
 package sample.Utilities;
 
-import com.sun.javafx.geom.Vec3f;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import sample.Config.Colors;
+import javafx.scene.image.Image;
+import javafx.scene.shape.Circle;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
 
 import java.util.Vector;
 
 import static org.opencv.imgproc.Imgproc.*;
 
 public class CircleServices {
-
-
-
-    private void reduceNoise(Mat src, Mat srcGray){
-        cvtColor(src, srcGray, COLOR_BGR2HSV);
-
+    private void reduceNoise(Mat src, Mat srcGray) {
+        cvtColor(src, srcGray, Imgproc.COLOR_RGBA2GRAY);
+        GaussianBlur(srcGray, srcGray, new Size(9, 9), 2, 2);
     }
 
-    public void findCircles(Mat src){
-        Scalar minColor = new Scalar(50, 32, 16);
-        Scalar maxColor = new Scalar(80,245,245);
+
+    public Mat findCircles(Mat src) {
         Mat srcGray = new Mat();
-        Core.inRange(src, minColor, maxColor, srcGray);
-        reduceNoise(src,srcGray);
+        reduceNoise(src, srcGray);
         Mat circles = new Mat();
-        HoughCircles(srcGray, circles, CV_HOUGH_GRADIENT, 1, 100, 4, 6, 16, 65);
-        System.out.println(circles);
+        Vector<Mat> circlesList = new Vector<Mat>();
+
+        HoughCircles(srcGray, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 60, 200, 20, 30, 0);
+        System.out.println("#rows " + circles.rows() + " #cols " + circles.cols());
+        double x = 0.0;
+        double y = 0.0;
+        int r = 0;
+
+        for (int i = 0; i < circles.rows(); i++) {
+            double[] data = circles.get(i, 0);
+            for (int j = 0; j < data.length; j++) {
+                x = data[0];
+                y = data[1];
+                r = (int) data[2];
+            }
+            Point center = new Point(x, y);
+            circle(srcGray, center, 3, new Scalar(0, 255, 0), -1, 8, 0);
+            circle(srcGray, center, r, new Scalar(0, 0, 255), 3, 8, 0);
+        }
+        System.out.println(circlesList);
+        return srcGray;
+
     }
+
 }
